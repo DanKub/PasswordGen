f9952993a6272edcb3a6c6fe60aa699bb3814ca7
DanKub <DanKub@users.noreply.github.com>
2017-10-19 19:51:33 +0200

    first simple popup functionality

--- sources/defaults/preferences/prefs.js
+++
@@ -1,9 +0,0 @@
-pref("extensions.passwordgen.b64Enc", true);
-pref("extensions.passwordgen.hexEnc", false);
-pref("extensions.passwordgen.lengthPass", "15");
-pref("extensions.passwordgen.isActive", true);
-pref("extensions.passwordgen.passShowTime", "0");
-pref("extensions.passwordgen.useStored", true);
-pref("extensions.passwordgen.store", true);
-pref("extensions.passwordgen.const", "");
-pref("extensions.passwordgen.toolbar-button-set",false);
