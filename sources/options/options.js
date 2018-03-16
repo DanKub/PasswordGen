c8a0dc0476defdca66c4a1a0b7f7d960481542ee
DanKub <DanKub@users.noreply.github.com>
2018-03-16 19:01:39 +0100

    indent child rules in table + delete stored rules functionality

--- sources/options/options.js
+++ sources/options/options.js
@@ -65,10 +65,31 @@ function loadTableRules(){
     deleteTableRules();
     reqCursor.onsuccess = function() {
         var cursor = reqCursor.result;
-        if(cursor) {
-            insertNewTableEntry(cursor.value.domain, Number(cursor.value.pwdLength), cursor.value.b64Enc == "true" , cursor.value.hexEnc == "true", cursor.value.pdl);
+        if(cursor){
+            // Ak ma zaznam deti -> je rodic
+            if(cursor.value.childs){
+                // Do tabulky vloz najprv rodica
+                insertNewTableEntry(cursor.value.domain, Number(cursor.value.pwdLength), cursor.value.b64Enc == "true" , cursor.value.hexEnc == "true", cursor.value.pdl, cursor.value.parent != null);
+                
+                var reqPdl = index.getAll(cursor.value.pdl);
+                reqPdl.onsuccess = function(){
+                    // Do tabulky vloz postupne vsetky deti, ktore ma rodic
+                    for(var i = 0; i < cursor.value.childs.length; i++){
+                        for(var j = 0; j < reqPdl.result.length; j++){
+                            if (cursor.value.childs[i] == reqPdl.result[j].domain){
+                                insertNewTableEntry(reqPdl.result[j].domain, Number(reqPdl.result[j].pwdLength), reqPdl.result[j].b64Enc == "true" , reqPdl.result[j].hexEnc == "true", reqPdl.result[j].pdl, reqPdl.result[j].parent != null);
+                            }
+                        }
+                    }
+                }
+            }
+            // Ak zaznam nema rodica
+            else if(cursor.value.parent == null) {
+                insertNewTableEntry(cursor.value.domain, Number(cursor.value.pwdLength), cursor.value.b64Enc == "true" , cursor.value.hexEnc == "true", cursor.value.pdl, cursor.value.parent != null);
+            }
             cursor.continue();
-        } else {
+        }
+        else {
             console.log('Entries all displayed.');    
         }
     }
@@ -103,7 +124,7 @@ function validatePdl(pdl){
 function updateUIChildRules(ruleChilds, parentNode){
     for (var i = 0; i < ruleChilds.length; i++){
         for (var j = 1; j < table.rows.length; j++){
-            if (table.rows[j].children[0].firstChild.value == ruleChilds[i]){
+            if (table.rows[j].children[0].firstChild.data == ruleChilds[i]){
                 table.rows[j].children[1].firstChild.value = parentNode.cells[1].firstChild.value;
                 table.rows[j].children[2].firstChild.checked = parentNode.cells[2].firstChild.checked;
                 table.rows[j].children[3].firstChild.checked = parentNode.cells[3].firstChild.checked;
@@ -126,7 +147,7 @@ function updateStoredRule(event){
     var rulesObjStore = transaction.objectStore("rules");
     var indexDomain = rulesObjStore.index("domain");
 
-    var reqDomain = indexDomain.get(node.cells[0].firstChild.value);
+    var reqDomain = indexDomain.get(node.cells[0].firstChild.data);
     reqDomain.onsuccess = function(){
         var storedRule = reqDomain.result;
 
@@ -134,14 +155,14 @@ function updateStoredRule(event){
         if (storedRule.childs == null){
             //Ak nema deti a ma rodica, tak zmenim iba dany (detsky) zaznam a oddelim ho od hlavneho stromu ako novy strom
             if(storedRule.parent){
-                rulesObjStore.put({ domain: node.cells[0].firstChild.value, 
+                rulesObjStore.put({ domain: node.cells[0].firstChild.data, 
                                     pwdLength: node.cells[1].firstChild.value,
                                     b64Enc: String(node.cells[2].firstChild.checked),
                                     hexEnc: String(node.cells[3].firstChild.checked),
-                                    pdl: node.cells[0].firstChild.value, // zmena pdl na domain name -> takto vznikne novy strom
-                                    }, node.cells[0].firstChild.value);
+                                    pdl: node.cells[0].firstChild.data, // zmena pdl na domain name -> takto vznikne novy strom
+                                    }, node.cells[0].firstChild.data);
 
-                node.cells[4].firstChild.value = node.cells[0].firstChild.value; // prepisem pld zaznam v UI
+                node.cells[4].firstChild.value = node.cells[0].firstChild.data; // prepisem pld zaznam v UI
 
                 // Odstranenie zaznamu od rodica (nakolko zo zmeneneho zaznamu je teraz samostatny strom)
                 // vyziadam si jeho rodica a odstranim ho odtial
@@ -175,25 +196,25 @@ function updateStoredRule(event){
 
             //Ak zaznam, ktory idem menit nema deti ani rodica
             else{
-                rulesObjStore.put({ domain: node.cells[0].firstChild.value, 
+                rulesObjStore.put({ domain: node.cells[0].firstChild.data, 
                                     pwdLength: node.cells[1].firstChild.value,
                                     b64Enc: String(node.cells[2].firstChild.checked),
                                     hexEnc: String(node.cells[3].firstChild.checked),
                                     pdl: node.cells[4].firstChild.value,
-                                    }, node.cells[0].firstChild.value);
+                                    }, node.cells[0].firstChild.data);
             }
         }
 
         // Zaznam ktory idem menit MA DETI. Treba ich vsetky zmenit podla rodica (aktualneho zaznamu).
         else {
             // Zmena aktualneho (rodicovskeho) zaznamu, ktory user zmenil cez GUI
-            rulesObjStore.put({ domain: node.cells[0].firstChild.value, 
+            rulesObjStore.put({ domain: node.cells[0].firstChild.data, 
                                 pwdLength: node.cells[1].firstChild.value,
                                 b64Enc: String(node.cells[2].firstChild.checked),
                                 hexEnc: String(node.cells[3].firstChild.checked),
                                 pdl: node.cells[4].firstChild.value,
                                 childs: storedRule.childs,
-                                }, node.cells[0].firstChild.value);
+                                }, node.cells[0].firstChild.data);
 
             updateUIChildRules(storedRule.childs, node); // zmena detskych zaznamov v tabulke GUI
 
@@ -211,13 +232,82 @@ function updateStoredRule(event){
     }
 }
 
-function insertNewTableEntry(domain, pwdLength, base64Check, hexCheck, pdl){
+function deleteStoredRule(event){
+    var node = event.target;
+    while(node.nodeName != "TR"){   //preiterujem sa na riadok v tabulke, v ktorom nastala nejaka zmena
+        node = node.parentElement;
+    }
+
+    var transaction = genRulesDB.transaction(["rules"], "readwrite");
+    var rulesObjStore = transaction.objectStore("rules");
+    var indexDomain = rulesObjStore.index("domain");
+
+    var reqDomain = indexDomain.get(node.cells[0].firstChild.data);
+    reqDomain.onsuccess = function(){
+        // Ak zaznam, ktory chcem vymazat je rodic
+        if(reqDomain.result.childs){
+            for(var i = 0; i < reqDomain.result.childs.length; i++){
+                rulesObjStore.delete(reqDomain.result.childs[i]); // Vymazanie detskych zaznamov v DB
+
+                for (var j = 1; j < table.rows.length; j++){
+                    if (table.rows[j].children[0].firstChild.data == reqDomain.result.childs[i]){
+                        table.rows[j].remove(); // Vymazanie detskych zaznamov v tabulke GUI
+                    }
+                }
+            }
+            rulesObjStore.delete(reqDomain.result.domain); // Vymazanie rodica v DB
+            node.remove(); // Vymazanie rodica z tabulky GUI
+        }
+
+        // Ak zaznam ktory chcem vymazat je dieta
+        else if(reqDomain.result.parent){
+            // Detsky zaznam bude vymazany a v jeho rodicovi ho teda musim odstranit zo zoznamu deti
+            var reqParentDomain = indexDomain.get(reqDomain.result.parent);
+            reqParentDomain.onsuccess = function(){
+                var storedParentRule = reqParentDomain.result;
+                var parentChilds = storedParentRule.childs;
+                parentChilds.splice(parentChilds.indexOf(reqDomain.result.domain), 1);
+
+                // Ak rodic po vymazani childa obsahuje este ine deti
+                if(storedParentRule.childs.length > 0){
+                    rulesObjStore.put({ domain: storedParentRule.domain, 
+                                        pwdLength: storedParentRule.pwdLength,
+                                        b64Enc: storedParentRule.b64Enc,
+                                        hexEnc: storedParentRule.hexEnc,
+                                        pdl: storedParentRule.pdl,
+                                        childs: storedParentRule.childs,
+                                        }, storedParentRule.domain);
+                }
+                // Ak to bol posledny detsky zaznam, tak vymaz property 'childs'
+                else{
+                    rulesObjStore.put({ domain: storedParentRule.domain, 
+                                        pwdLength: storedParentRule.pwdLength,
+                                        b64Enc: storedParentRule.b64Enc,
+                                        hexEnc: storedParentRule.hexEnc,
+                                        pdl: storedParentRule.pdl,
+                                        }, storedParentRule.domain);
+                }
+                rulesObjStore.delete(reqDomain.result.domain); // Vymazanie zaznamu v DB
+            }
+            node.remove(); // Vymazanie zaznamu v tabulke GUI
+        }
+
+        // Zaznam nema rodica ani dieta
+        else{
+            rulesObjStore.delete(reqDomain.result.domain);
+            node.remove();
+        }
+    }
+}
+
+function insertNewTableEntry(domain, pwdLength, base64Check, hexCheck, pdl, isChild){
     var row = table.insertRow();
     var domainCell = row.insertCell();
     var pwdLengthCell = row.insertCell();
     var base64Cell = row.insertCell();
     var hexCell = row.insertCell();
     var pdlCell = row.insertCell();
+    var delIconCell = row.insertCell();
 
     // pwdLengthCell.contentEditable = "true";
     // pdlCell.contentEditable = "true";
@@ -227,26 +317,33 @@ function insertNewTableEntry(domain, pwdLength, base64Check, hexCheck, pdl){
     radioBase64Input.setAttribute("name", "encoding_row" + row.rowIndex);
     var radioHexInput = radioBase64Input.cloneNode(true);
 
-    var domainCellInput = document.createElement("input");
-    domainCellInput.type = "text";
-    domainCellInput.pattern = "([[a-z0-9]+\.)*[a-z0-9]+\.[a-z0-9]+";
-    var pdlCellInput = domainCellInput.cloneNode(true);
-    domainCellInput.readOnly = true;
+    if (isChild){
+        domainCell.setAttribute("class", "children");
+    }
+
+    var pdlCellInput = document.createElement("input");
+    pdlCellInput.type = "text";
+    pdlCellInput.pattern = "([[a-z0-9]+\.)*[a-z0-9]+\.[a-z0-9]+";
 
     var pwdLengthCellInput = document.createElement("input");
     pwdLengthCellInput.type = "text";
 
-    domainCellInput.value = domain;
+    var delIcon = document.createElement("i");
+    delIcon.className = "material-icons";
+    delIcon.innerText = "delete";
+    delIcon.addEventListener("click", deleteStoredRule);
+
+    domainCell.innerHTML = domain;
     pwdLengthCellInput.value = pwdLength;
     radioBase64Input.checked = base64Check;
     radioHexInput.checked = hexCheck;
     pdlCellInput.value = pdl;
 
-    domainCell.appendChild(domainCellInput);
     pwdLengthCell.appendChild(pwdLengthCellInput);
     base64Cell.appendChild(radioBase64Input);
     hexCell.appendChild(radioHexInput);
     pdlCell.appendChild(pdlCellInput);
+    delIconCell.appendChild(delIcon);
 }
 
 function openGenRulesDB(){
